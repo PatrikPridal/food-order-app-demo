23a8cf1c84dbd0d175a30b643c68e596b61ed3c9
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-09 21:44:16 +0100

    MealsSummary (dummy data)

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,21 +1,22 @@
-import { Fragment } from 'react';
-import HeaderCartButton from './HeaderCartButton';
+import { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
 //import image from 'react';
 
-import mealsImage from '../../assets/meals.jpg'
-import classes from './Header.module.css';
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
 const Header = (props) => {
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>ReactMeals</h1>
-            <HeaderCartButton />
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImage} alt="delicious food!" />
-        </div>
-
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt='delicious food!' />
+      </div>
     </Fragment>
+  );
 };
 
 export default Header;
