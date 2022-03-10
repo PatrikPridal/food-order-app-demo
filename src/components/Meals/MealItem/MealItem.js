37fb755a6f9323c8ba0390a6081efda406866a25
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-10 16:20:45 +0100

    added form for adding items into cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,6 @@
+import MealItemForm from './MealItemForm';
 import classes from "./MealItem.module.css";
+
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -10,7 +12,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
