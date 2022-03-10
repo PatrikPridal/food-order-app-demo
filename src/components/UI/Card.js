d453ab5b95d1d6dbc5f3502798d6d328e546e8ca
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-10 15:46:53 +0100

    Card component

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,5 +1,7 @@
+import classes from './Card.module.css';
 
-
-const Card = () => {};
+const Card = props => {
+    return <div className={classes.card}>{props.children}</div>
+};
 
 export default Card;
