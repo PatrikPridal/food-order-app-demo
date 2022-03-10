6b2aea5008b0ac59ad01392198904486ffbf0257
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-10 18:46:10 +0100

    .

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -8,7 +8,7 @@ const HeaderCartButton = (props) => {
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>1</span>
     </button>
   );
 };
