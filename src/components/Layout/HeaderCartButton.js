10fc10ccc732c263e180e3f16279d3c3e9d20ed2
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-11 16:39:30 +0100

    logic and useEffect for handling bump "classes.bump" effect in css module

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,20 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../components/store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHiglighted, setBtnIsHiglighted] = useState(false);
   const cartCtx = useContext(CartContext); // header button will be rendered whenever context changes
 
+  const { items } = cartCtx; //destructure so I can add "items" into useEffect hook as dependencies
   //reduce method for transform array of data into single value (number in this case)
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+
+  const btnClasses = `${classes.button} ${btnIsHiglighted ? classes.bump : ''}`;
+
+  useEffect(() =>{
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHiglighted(true); // every add into cart setting bump classes on btn on
+
+    //timeout for 300ms (as long that effect is) then set that effect to false
+    setTimeout(() => {
+      setBtnIsHiglighted(false);
+    }, 300);
+  }, [items]); //dependencies -> [items]
+
   // passing "numberOfCartItems into span for number of cart items in it"
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
