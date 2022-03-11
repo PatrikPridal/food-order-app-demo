9613ea685b5e54726699e258f8f66049325361fa
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-11 13:36:36 +0100

    CartProvider component for return jsx CartContext.Provider

---
+++ src/components/store/CartProvider.js
@@ -0,0 +1,11 @@
+import CartContext from './cart-context';
+
+
+const CartProvider = props => {
+    return <CartContext.Provider>
+        {props.children}
+    </CartContext.Provider>
+};
+
+
+export default CartProvider;
