34980488f2eb9905413ea0413be5dfa6ceed2a61
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-11 15:33:57 +0100

    condition "order" button in cart showed only when something is in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,6 +6,10 @@ import CartContext from "../../components/store/cart-context";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
@@ -19,13 +23,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>} 
       </div>
     </Modal>
   );
